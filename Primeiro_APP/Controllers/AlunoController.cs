0a2d5b46209a8584a2fa6711973077e9b8bbd625
MURILO ANJOS SOARES <00001114793838sp@aluno.educacao.sp.gov.br>
2026-03-18 17:58:56 -0300

    update Action Teste, Resultado

--- Primeiro_APP/Controllers/AlunoController.cs
+++ Primeiro_APP/Controllers/AlunoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Pkcs;
 
 namespace Primeiro_APP.Controllers
 {
@@ -6,8 +7,19 @@ namespace Primeiro_APP.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.nota = "Aluno Nota 10";
+            ViewBag.nota = "Aluno Anhanguera MB";
             ViewData["Escola"] = "E.E. Anhanguera";
+            return View();
+        }
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+        public IActionResult Resultado()
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Aluno Anhanguera MB";
             return View();
         }
     }
